aed395eae81f2af4bf92ce100215c24babf352ef
taleshrocha <taleshrochaz@gmail.com>
2021-06-27 12:10:03 -0300

    Reverse done

--- reverse/include/reverse.h
+++ reverse/include/reverse.h
@@ -1,3 +1,4 @@
+//! \file reverse.h
 #ifndef GRAAL_H
 #define GRAAL_H
 
@@ -9,15 +10,23 @@ using std::distance;
 using std::sort;
 
 namespace graal {
-
-/*! 
- * TODO: documentação no estilo doxygen
- */
-template<class BidirIt>
-void reverse(BidirIt first, BidirIt last)
-{
-    // TODO
-}
-
+    /*! 
+     * \param first Points to the first element of the array.
+     * \param last Points to the last element of the array.
+     */
+    template<class BidirIt>
+    void reverse(BidirIt first, BidirIt last){
+        auto dist = distance(first, last);
+        last--;
+        if(!(dist%2)){
+            for(int i = 0; i < dist/2; ++i){
+                std::iter_swap(first++, last--);
+            }
+        }
+        else{
+            while(first != last and first != last-1 and first != 0)
+                std::iter_swap(first++, last--);
+        }
+    }
 }
 #endif
