c6857f479e56b18bb9edba155d69bbff8ee3434c
taleshrocha <taleshrochaz@gmail.com>
2021-06-26 23:26:29 -0300

    find_if done

--- find_if/include/find_if.h
+++ find_if/include/find_if.h
@@ -1,3 +1,4 @@
+// \file find_if.h
 #ifndef GRAAL_H
 #define GRAAL_H
 
@@ -10,15 +11,20 @@ using std::sort;
 
 namespace graal {
 
-/*! 
- * TODO: documentação no estilo doxygen
- */
-template<class InputIt, class UnaryPredicate>
-InputIt find_if(InputIt first, InputIt last, UnaryPredicate p)
-{
-    // TODO
-    return first;
-}
-
+    /*! 
+     * \brief This function return the first element in the array that makes the predicate return
+     * true.
+     * \param first points to the first element of the array.
+     * \param last points to the last element of the array.
+     * \param p A boolean function.
+     */
+    template<class InputIt, class UnaryPredicate>
+    InputIt find_if(InputIt first, InputIt last, UnaryPredicate p){
+        while(first != last){
+            if(p(*first)) return first;
+            first++;
+        }
+        return last;
+    }
 }
 #endif
