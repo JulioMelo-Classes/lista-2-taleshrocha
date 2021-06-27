11701a73cc20ad7b29050ff14657f886e4ab2508
taleshrocha <taleshrochaz@gmail.com>
2021-06-26 23:13:44 -0300

    find done

--- find/include/find.h
+++ find/include/find.h
@@ -1,3 +1,4 @@
+//! \file find.h
 #ifndef GRAAL_H
 #define GRAAL_H
 
@@ -10,15 +11,21 @@ using std::sort;
 
 namespace graal {
 
-/*! 
- * TODO: documentação no estilo doxygen
- */
-template<class InputIt, class T, class Equal>
-InputIt find(InputIt first, InputIt last, const T& value, Equal eq)
-{
-    // TODO
-    return first;
-}
-
+    /*! 
+     * \brief This function returns the pointer for the value of the array which is equal to the
+     * value variable.
+     * \param first Points to the arrys's first element.
+     * \param last Points to the arrys's last element.
+     * \param value Value to be compared in the function. 
+     * \param Equal A function that return true when both of it's parameter are equal.
+     */
+    template<class InputIt, class T, class Equal>
+    InputIt find(InputIt first, InputIt last, const T& value, Equal eq){
+        while(first != last){
+            if(*first == value) return first;
+            first++;
+        }
+        return last;
+    }
 }
 #endif
