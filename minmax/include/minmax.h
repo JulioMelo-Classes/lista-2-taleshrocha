305bf06c558d73f4fc0a9f7d30211f644c53123a
taleshrocha <taleshrochaz@gmail.com>
2021-06-27 00:42:04 -0300

    minmax done

--- minmax/include/minmax.h
+++ minmax/include/minmax.h
@@ -1,3 +1,4 @@
+// \file minmax.h
 #ifndef GRAAL_H
 #define GRAAL_H
 
@@ -10,25 +11,30 @@ using std::sort;
 
 namespace graal {
 
-/*! 
- * Exemplo de documentação seguindo o formato Doxygen
- *
- * @tparam Itr iterator para o range.
- * @tparam Compare o tipo de uma função bool(const T &a, const T &b)
- *
- * @param first Ponteiro para o primeiro elemento do range
- * @param last Ponteiro para a posição logo após o último elemento do range
- * @param cmp A função de comparação que retorna true quando o primeiro parâmetro é menor do que o segundo
- *
- * @return Um std::pair contendo o menor e maior elemento, nesta ordem
- *
- */
-template <typename Itr, typename Compare >
-std::pair<Itr, Itr> minmax( Itr first, Itr last, Compare cmp )
-{
-    // TODO
-    return std::make_pair( first, first );
-}
+    /*! 
+     * Exemplo de documentação seguindo o formato Doxygen
+     *
+     * @tparam Itr iterator para o range.
+     * @tparam Compare o tipo de uma função bool(const T &a, const T &b)
+     *
+     * @param first Ponteiro para o primeiro elemento do range
+     * @param last Ponteiro para a posição logo após o último elemento do range
+     * @param cmp A função de comparação que retorna true quando o primeiro parâmetro é menor do que o segundo
+     *
+     * @return Um std::pair contendo o menor e maior elemento, nesta ordem
+     *
+     */
+    template <typename Itr, typename Compare >
+    std::pair<Itr, Itr> minmax( Itr first, Itr last, Compare cmp ){
+        Itr min, max;
+        min = max = first;
 
+        while(first != last){
+            if(!cmp(*first, *max)) max = first;
+            if(cmp(*first, *min)) min = first;
+            first++;
+        }
+        return std::make_pair(min, max);
+    }
 }
 #endif
