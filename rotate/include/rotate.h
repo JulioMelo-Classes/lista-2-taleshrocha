f7ac1c808aede320e8ca4cd94beb928048990137
taleshrocha <taleshrochaz@gmail.com>
2021-06-27 14:18:14 -0300

    rotate done

--- rotate/include/rotate.h
+++ rotate/include/rotate.h
@@ -1,3 +1,4 @@
+//! \file rotate.h
 #ifndef GRAAL_H
 #define GRAAL_H
 
@@ -9,16 +10,35 @@ using std::distance;
 using std::sort;
 
 namespace graal {
+    /*! 
+     * TODO: documentação no estilo doxygen
+     */
+    template<class ForwardIt>
+    ForwardIt rotate(ForwardIt first, ForwardIt n_first, ForwardIt last){
+        ForwardIt aux = n_first;
+        if(n_first == first) return first;
+        while(n_first != last){
+            std::iter_swap(n_first++, first++);
+        }
+        auto dist = distance(first, last) + 1;
+        if(!(dist%2)){
+            n_first = first+(dist/2);
+            while(n_first != last){
+                std::iter_swap(n_first++, first++);
+            }
+            n_first = last - (dist/3);
+            while(n_first != last){
+                std::iter_swap(n_first++, first++);
+            }
+        }
+        else{
+            n_first = first+1;
+            while(n_first != last){
+                std::iter_swap(n_first++, first++);
+            }
+        }
 
-/*! 
- * TODO: documentação no estilo doxygen
- */
-template<class ForwardIt>
-ForwardIt rotate(ForwardIt first, ForwardIt n_first, ForwardIt last)
-{
-    // TODO
-   return last;
-}
-
+        return aux;
+    }
 }
 #endif
